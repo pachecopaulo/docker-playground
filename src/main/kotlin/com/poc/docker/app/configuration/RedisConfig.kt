dd28ebb5fb69c38cfe9cffe1c366e77e1d3f21a5
Paulo Eduardo Pacheco <eduardoguitar@gmail.com>
2018-12-01 06:07:09 +0100

    docker compose

--- src/main/kotlin/com/poc/docker/app/configuration/RedisConfig.kt
+++ src/main/kotlin/com/poc/docker/app/configuration/RedisConfig.kt
@@ -1,7 +1,10 @@
 package com.poc.docker.app.configuration
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 
@@ -9,11 +12,20 @@ import org.springframework.data.redis.core.RedisTemplate
 class RedisConfig {
 
     @Bean
-    fun redisConnectionFactory() = LettuceConnectionFactory()
+    fun redisConnectionFactory() = LettuceConnectionFactory(
+        RedisStandaloneConfiguration().apply {
+            hostName = redisProperties().host
+            port = redisProperties().port
+        }
+    )
 
     @Bean
     fun redisTemplate() =
         RedisTemplate<String, Long>().apply {
             setConnectionFactory(redisConnectionFactory())
         }
+
+    @Bean
+    @Primary
+    fun redisProperties() = RedisProperties()
 }
